bd606d9a212738622bec1e4c99c2f705caa2a21f
Titik Nurfaidah <nurfaidahtitik@gmail.com>
2016-12-11 22:45:39 +0700

    resolve show data by id

--- getDataByUang.php
+++ getDataByUang.php
@@ -1,7 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 $connect = mysqli_connect("localhost","root","","konversi");
-$result = mysqli_query($connect, "SELECT * FROM datauang");
+$result = mysqli_query($connect, "SELECT * FROM datauang ");
 $data = array();
 // if(!$result){
 // 	printf("Error: %s\n", mysqli_error($connect));
